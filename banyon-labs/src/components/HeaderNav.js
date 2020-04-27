8e29502b00411a70b189b7e011812942cbef6348
AlexM890 <55465266+AlexM890@users.noreply.github.com>
2020-04-26 19:06:17 -0500

    BLW-4 added WhoWeAre section

--- banyon-labs/src/components/HeaderNav.js
+++ banyon-labs/src/components/HeaderNav.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Navbar, Nav, NavDropdown, Image } from 'react-bootstrap'
 import Logo from '../styles/images/Logo.png'
 
-const HeaderNav = () => (
+const HeaderNav = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const handleOpen = () => {
+        setIsOpen(true)
+    }
+    
+    const handleClose = () => {
+        setIsOpen(false)
+    }
+
+      return (
         <div>
             <Navbar bg="light" expand="lg">
                 <Navbar.Brand href="/"><Image className="logo" src={Logo} /></Navbar.Brand>
@@ -10,13 +21,15 @@ const HeaderNav = () => (
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
                         <Nav.Link href="/">Home</Nav.Link>
-                        <NavDropdown title="Who We Are" id="basic-nav-dropdown1">
+                        <Nav.Link href="/">
+                        <NavDropdown onMouseEnter={() => handleOpen()} onMouseLeave={handleClose} noCaret open={isOpen} flip={true} title="Who We Are" id="basic-nav-dropdown1">
                             <NavDropdown.Item href="/about_us">About</NavDropdown.Item>
                             <NavDropdown.Item href="#">Mission</NavDropdown.Item>
                             <NavDropdown.Item href="#">Team</NavDropdown.Item>
                             <NavDropdown.Item href="#">Partners</NavDropdown.Item>
                             <NavDropdown.Divider/>
                         </NavDropdown>
+                        </Nav.Link>
                         <NavDropdown title="What We Do" id="basic-nav-dropdown2">
                             <NavDropdown.Item href="#">Our Process</NavDropdown.Item>
                             <NavDropdown.Item href="#">Web Apps</NavDropdown.Item>
@@ -28,6 +41,7 @@ const HeaderNav = () => (
                 </Navbar.Collapse>
             </Navbar>
         </div>
-)
+      )
+}
 
 export default HeaderNav
---
+++ banyon-labs/src/components/WhoWeAre.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Zoom } from 'react-reveal'
+
+export default function WhoWeAre() {
+    return (
+        <div className="row justify-content-center mt-5">
+            <div className="text-center">
+                <h3 className="display-4">Who We Are</h3>
+            </div>
+            <Zoom>
+                
+            </Zoom>
+        </div>
+    )
+}
