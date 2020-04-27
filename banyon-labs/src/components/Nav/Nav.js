b4ed28b38962353bcc9a62d128a3e5ad863c1404
Mark Langhamer <langhamerm@gmail.com>
2020-04-27 12:16:05 -0500

    Fixed Nav and Logo

--- banyon-labs/src/components/Nav/Nav.js
+++ banyon-labs/src/components/Nav/Nav.js
@@ -1,44 +1,44 @@
 import React, { Component } from "react";
 import { Container, Row, Col, Navbar } from "react-bootstrap";
-// import './Nav.css';
+import './Nav.css';
 class Nav extends Component {
   render() {
     return (
   
-        <Navbar bg="dark" variant="dark">
-          <Navbar.Brand href="#home">
-          <img
-        alt="logo"
-        src="/banyan.svg"
-        width="50"
-        height="50"
-        // className="d-inline-block align-top"
-      />
+      //   <Navbar bg="dark" variant="dark">
+      //     <Navbar.Brand href="#home">
+      //     <img
+      //   alt="logo"
+      //   src="/banlabs.png"
+      //   width="50"
+      //   height="50"
+      //   // className="d-inline-block align-top"
+      // />
           
-          </Navbar.Brand>
-        </Navbar>
+      //     </Navbar.Brand>
+      //   </Navbar>
   
 
-//      <Container>
-//   <nav>
-//     <div className="nav-wrapper">
-//       <a href="#" className="brand-logo">
-    
-//       </a>
-//       <ul id="nav-mobile" className="right hide-on-med-and-down">
-//         <li>
-//           <a href="sass.html">About</a>
-//         </li>
-//         <li>
-//           <a href="badges.html">Services</a>
-//         </li>
-//         <li>
-//           <a href="collapsible.html">Contact</a>
-//         </li>
-//       </ul>
-//     </div>
-//   </nav>
-// </Container> 
+     <Container>
+  <nav>
+    <div className="nav-wrapper">
+
+      <a href="#" className="brand-logo">Home
+      </a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li>
+          <a href="sass.html">About</a>
+        </li>
+        <li>
+          <a href="badges.html">Services</a>
+        </li>
+        <li>
+          <a href="collapsible.html">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</Container> 
     );
   }
 }
