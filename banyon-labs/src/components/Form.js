7d11da3984876495c15fe59a55935827f66f839c
Kenneth007G <59965274+Kenneth007G@users.noreply.github.com>
2020-05-04 09:22:32 -0500

    Update Form.js

--- banyon-labs/src/components/Form.js
+++ banyon-labs/src/components/Form.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import {Form, Button, Col} from "react-bootstrap";
-
+const axios = require('axios');
 
 
 const ContactForm = (props) => {
-  // const style = {
-  //   width: '150px',
-  //   height: '150px'
-  // }
+const [ state, setState ] = useState({email: "", name: "", platform: ""})
+const onSubmit = (event) => {
+      event.preventDefault();
+    axios
+      .post(
+        "https://https://getform.io/thank-you",
+        state,
+        { headers: { Accept: "application/json" } }
+      )
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  };
+
+
+
   return (
     <div id="contact_us" className="outterMainDiv w-100">
       <div className="p-4 innerMainDiv col-9">
@@ -15,28 +30,40 @@ const ContactForm = (props) => {
           <h2 className="row justify-content-center mt-3 ml-5 mr-5 mb-5 p-3 getInTouch border border-white">Get in Touch</h2>
         </div>       
 
-      <Form>
+      <Form onSubmit={onSubmit} action="https://getform.io/f/9ded9583-4090-4627-b4e4-dc8228494fd7" encType="multipart/form-data" method="POST">
         <Form.Group controlId="formGridName">
           <Form.Label className="text-white">Name:</Form.Label>
-          <Form.Control placeholder="Enter Name" />
+          <Form.Control placeholder="Enter Name" onChange={(e) => setState({
+            ...state,
+            name: e.target.value
+          })} required />
         </Form.Group>
 
         <Form.Row>
           <Form.Group as={Col} controlId="formGridEmail">
             <Form.Label className="text-white">Email:</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control type="email" placeholder="Enter email" onChange={(e) => setState({
+              ...state,
+              email: e.target.value
+            })} required />
           </Form.Group>
         </Form.Row>
 
         <Form.Group controlId="formGridPhone">
           <Form.Label className="text-white">Phone:</Form.Label>
-          <Form.Control placeholder="Enter Phone Number" />
+          <Form.Control placeholder="Enter Phone Number" onChange={(e) => setState({
+            ...state,
+            phone: e.target.value
+          })} />
         </Form.Group>
 
         <Form.Row>
           <Form.Group controlId="exampleForm.ControlTextarea1">
             <Form.Label className="text-white">Additional message:</Form.Label>
-            <Form.Control className="w-100" cols="140" as="textarea" rows="5" placeholder="Add a brief message" />
+            <Form.Control className="w-100" cols="140" as="textarea" rows="5" placeholder="Add a brief message" onChange={(e) => setState({
+              ...state,
+              message: e.target.value
+            })} />
           </Form.Group>
         </Form.Row>
 
