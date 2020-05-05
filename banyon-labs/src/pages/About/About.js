2704a488a871b34fd103ff4c9637604cb25ece24
MColbertPersevere <mcolbert@sharedservices.io>
2020-05-05 13:13:31 -0500

    updated about us, made navbar more transparent, took out spacing div in approuter, added margin to carousel

--- banyon-labs/src/pages/About/About.js
+++ banyon-labs/src/pages/About/About.js
@@ -1,25 +1,25 @@
 import React, { Component } from "react";
-import ContactForm from "../../components/Form";
-import { Container, Row} from "react-bootstrap";
-import BackgroundImage from "../../styles/images/aboutBackground1.jpg"
+import { Container, Row } from "react-bootstrap";
 import Divider from '@material-ui/core/Divider';
+import './about.css'
+import Carousel2 from "../../components/Carousel2";
+
 export default class About extends Component {
   render() {
     return (
       <div
         style={{
-          color: "white",
+          // color: "white",
           "font-family": "bc-alphapipe, sans-serif",
-          fontSize: "22px",
+          fontSize: "20px",
         }}
       >
-        <Row
+        <Row className="justify-content-center"
         // style= // {{ backgroundColor: "#24234d" }}
         >
           {" "}
           {/*Might be better to have a bacgrkound image instead of the color*/}
-            <Container className=" pt-4 pb-4 text-left" 
-            style= {{backgroundImage: `url(${BackgroundImage})`, height:"400px", width:"60vw"}}
+            {/* <Container className=" pt-4 pb-4 text-left"
             >
 
               <h1
@@ -28,21 +28,20 @@ export default class About extends Component {
               >
                 About Us
               </h1>
-            </Container>
-          
+            </Container> */}
+           <Carousel2 />
         </Row>
         <Row>
           {/*Adding in some more text to see how things space out. Once we recevie the final text may need to be restyled.*/}
           <Container
-            className="py-4 px-5 text-center"
-            style={{ color: "black", width:"60vw" }}
-          >
+            className="py-4 px-4 text-center col-sm-12 col-md-7"
+            style={{ color: "black" }}>
             <Container className="py-4 px-4 mb-5 text-center bg-light">
               <p>
                 Banyan Labs is U.S. based, international for-profit development
                 company created to provide high quality IT services while also
-                making a positive contribution to the effort to reduce recidivsm
-                and empower justice involved indiviuals to succeed in well-paying
+                making a positive contribution to the effort to reduce recidivism
+                and empower justice involved individuals to succeed in well-paying
                 jobs in the tech industry.
                 <br />
                 Co-located with Persevere, Banyan Labs employs Persevere
@@ -50,40 +49,42 @@ export default class About extends Component {
                 developers. providing them with supervision, mentoring, support
                 and valuable experience to launch their careers as developers.
               </p>
-          </Container>
-          <Divider style={{height:"2px"}} />
-          <Container className="py-4 px-5 mt-5 text-center bg-light">
-            <p className="py-4 px-4">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-              ante sem, mollis eget ligula at, vehicula ultrices mauris. Aliquam
-              in condimentum erat. Sed viverra lacus quis efficitur convallis.
-              Maecenas pellentesque lacus nec nunc pulvinar, non posuere velit
-              mattis. Phasellus purus mi, feugiat quis purus sit amet,
-              consectetur venenatis mi. Fusce id erat nibh. Phasellus vitae
-              massa sed diam dapibus volutpat vel sit amet tellus. Orci varius
-              natoque penatibus et magnis dis parturient montes, nascetur
-              ridiculus mus. Cras faucibus sed eros a tincidunt. Morbi sagittis
-              sem lectus, at facilisis augue finibus a. Maecenas scelerisque
-              vehicula tellus laoreet malesuada aenean . vitae facilisis.
-              Curabitur vitae ornare quam. Sed ultrices lacus sit amet orci
-              molestie, quis faucibus arcu tempor. Phasellus et nulla diam.
-              Donec augue nisl, pellentesque ac eleifend nec, tincidunt sit amet
-              libero. Proin ut aliquam ex, eu ultrices nunc. Curabitur urna dui,
-              lacinia in quam non, fringilla interdum felis. Nunc sit amet orci
-              vitae nisi sollicitudin tempor ut vitae ipsum. Nulla luctus lorem
-              quis augue blandit, at interdum nibh aliquam. Vestibulum ante
-              ipsum primis in faucibus orci luctus et ultrices posuere cubilia
-              curae; Integer semper tortor magna, at tincidunt libero dignissim
-              eget. Nunc tempus finibus cursus. Nulla faucibus convallis leo non
-              elementum. In ut enim sed quam dignissim ultrices non nec nisi.
-              Quisque dapibus cursus mauris, non pharetra sem suscipit nec. Cras
-              in ornare ante. Cras neque arcu, bibendum atincidunt ac, vehicula
-              vitae lectus. Mauris viverra nec est eget dapibus. Proin sed
-              maximus mauris, sit amet ultrices mi. Ut sagittis dui a arcu
-              volutpat feugiat. Mauris luctus erat urna, vitae volutpat dui
-              luctus eu.
-            </p>
-          </Container>
+            </Container>
+
+            <Divider style={{height:"2px"}} />
+
+            <Container className="py-4 px-4 mt-5 text-center bg-light">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+                ante sem, mollis eget ligula at, vehicula ultrices mauris. Aliquam
+                in condimentum erat. Sed viverra lacus quis efficitur convallis.
+                Maecenas pellentesque lacus nec nunc pulvinar, non posuere velit
+                mattis. Phasellus purus mi, feugiat quis purus sit amet,
+                consectetur venenatis mi. Fusce id erat nibh. Phasellus vitae
+                massa sed diam dapibus volutpat vel sit amet tellus. Orci varius
+                natoque penatibus et magnis dis parturient montes, nascetur
+                ridiculus mus. Cras faucibus sed eros a tincidunt. Morbi sagittis
+                sem lectus, at facilisis augue finibus a. Maecenas scelerisque
+                vehicula tellus laoreet malesuada aenean . vitae facilisis.
+                Curabitur vitae ornare quam. Sed ultrices lacus sit amet orci
+                molestie, quis faucibus arcu tempor. Phasellus et nulla diam.
+                Donec augue nisl, pellentesque ac eleifend nec, tincidunt sit amet
+                libero. Proin ut aliquam ex, eu ultrices nunc. Curabitur urna dui,
+                lacinia in quam non, fringilla interdum felis. Nunc sit amet orci
+                vitae nisi sollicitudin tempor ut vitae ipsum. Nulla luctus lorem
+                quis augue blandit, at interdum nibh aliquam. Vestibulum ante
+                ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+                curae; Integer semper tortor magna, at tincidunt libero dignissim
+                eget. Nunc tempus finibus cursus. Nulla faucibus convallis leo non
+                elementum. In ut enim sed quam dignissim ultrices non nec nisi.
+                Quisque dapibus cursus mauris, non pharetra sem suscipit nec. Cras
+                in ornare ante. Cras neque arcu, bibendum atincidunt ac, vehicula
+                vitae lectus. Mauris viverra nec est eget dapibus. Proin sed
+                maximus mauris, sit amet ultrices mi. Ut sagittis dui a arcu
+                volutpat feugiat. Mauris luctus erat urna, vitae volutpat dui
+                luctus eu.
+              </p>
+            </Container>
           </Container>
         </Row>
         {/* <Row style={{ backgroundColor: "#511135" }}>
