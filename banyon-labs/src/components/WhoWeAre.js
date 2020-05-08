a0869943aaf8360995c60f3f72b81edc8e36132c
AlexM890 <55465266+AlexM890@users.noreply.github.com>
2020-05-08 14:37:06 -0500

    fixed team section

--- banyon-labs/src/components/WhoWeAre.js
+++ banyon-labs/src/components/WhoWeAre.js
@@ -4,6 +4,7 @@ import SS1 from '../styles/images/ss.webp'
 import PLogo from '../styles/images/persLogo.png'
 import NLogo from '../styles/images/naico.jpg'
 import { Link } from "react-router-dom"
+import Team from '../styles/images/team.jpg'
 
 export default function WhoWeAre() {
 
@@ -23,11 +24,13 @@ export default function WhoWeAre() {
                     <h4 className="header-size-2 pt-3 sec-txt">Associate Opportunity</h4>
                     <p className="text-justify lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores voluptates porro non alias? Quidem, repellendus. Iure odit officiis quas. Earum voluptatibus suscipit deserunt illum totam fugiat asperiores officia laborum qui! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate natus temporibus ipsum, eveniet magni nobis nesciunt laboriosam ex corporis, rem maxime ullam aliquam ipsam beatae debitis, voluptates consequuntur dolores non.</p>
                 </div>
-
-                <div className="mt-4">
-                    <Fade right>
+                <div>
                     <Link className="no-dec" to="/team"><h3 className="text-center display-4 prim-txt">Team</h3></Link>
-                    <h4 className="header-size-2 sec-txt">Managment</h4>
+                    <img className="med-pic" src={Team} alt="image"/>
+                </div>
+                {/* <div className="mt-4">
+                    
+                    <h4 className="header-size-2 sec-txt">Management</h4>
                     <div className="row justify-content-between">
                         <div className="card col">
                             <div className="card-body row justify-content-between">
@@ -81,9 +84,8 @@ export default function WhoWeAre() {
                                 <p className="col">Lorem ipsum e</p>
                             </div>
                         </div>
-                    </div>
-                    </Fade>    
-                </div>
+                    </div> 
+                </div> */}
                 <div className="col-12 text-center mt-3">
                 <Link className="no-dec" to="/partners"><h3 className="display-4 prim-txt">Partners</h3></Link>
                     <div className="row justify-content-around">
