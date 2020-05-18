35233712657d01ba5b1d1a4bc6547ad0277a3b0c
AlexM890 <55465266+AlexM890@users.noreply.github.com>
2020-05-18 13:14:19 -0500

    changed the Channel to #contact

--- src/components/tutorial.js
+++ src/components/tutorial.js
@@ -1,5 +1,4 @@
 const { WebClient } = require('@slack/web-api');
-
 console.log('Getting started with Node Slack SDK');
 const web = new WebClient('xoxb-934873283057-1115504716422-JaiJi84vGaNAMpqpeRmOmXI5');
 
@@ -8,7 +7,7 @@ const SlackInt =  ({name, phone, email, message}) => {
   try {
     // Use the `chat.postMessage` method to send a message from this app
     return web.chat.postMessage({
-      channel: '#websitedesign',
+      channel: '#contact',
       text: `Name: ${name} \n Email: ${email} \n phone: ${phone ? phone : "No Phone Number Attached"} \n Message: ${message ? message : "No Message Attached"}`,
     });
   } catch (error) {
