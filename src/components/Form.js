bc7a29c9d8c3e4839d28268d698f9477b4c345f5
AlexM890 <55465266+AlexM890@users.noreply.github.com>
2020-05-26 09:06:53 -0500

    changed the contact form to email

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,26 +1,34 @@
 import SlackInt from "./tutorial"
 import React, { useState } from "react";
 import {Form, Button, Col} from "react-bootstrap";
+import * as emailjs from 'emailjs-com'
 const axios = require('axios');
-
-
 const ContactForm = (props) => {
 const [ state, setState ] = useState({email: "", name: "", phone: "", message: ""})
 const onSubmit = (event) => {
       event.preventDefault();
-        SlackInt(state)
+        // SlackInt(state)
+        let templateParams = {
+          from_email: state.email,
+          to_name: '<contact@banyanlabs.io>',
+          from_name: state.name,
+          number: state.phone,
+          message_html: state.message,
+         }
+         emailjs.send(
+          'gmail',
+          'banyantest',
+           templateParams,
+          'user_4my3il3TVE5QvTZ2QqykP'
+         )
         setState({email: "", name: "", phone: "", message: ""})
   };
-
-
-
   return (
     <div id="contact_us" className="outterMainDiv w-100">
       <div className="p-4 innerMainDiv col-9">
         <div className="row justify-content-center">
           <h2 className="row justify-content-center mt-3 ml-5 mr-5 mb-5 p-3 getInTouch">Get in Touch</h2>
         </div>       
-
       <Form onSubmit={onSubmit}>
         <Form.Group controlId="formGridName">
           <Form.Label className="text-white">Name:</Form.Label>
@@ -29,7 +37,6 @@ const onSubmit = (event) => {
             name: e.target.value
           })} required />
         </Form.Group>
-
         <Form.Row>
           <Form.Group as={Col} controlId="formGridEmail">
             <Form.Label className="text-white">Email:</Form.Label>
@@ -39,7 +46,6 @@ const onSubmit = (event) => {
             })} required />
           </Form.Group>
         </Form.Row>
-
         <Form.Group controlId="formGridPhone">
           <Form.Label className="text-white">Phone:</Form.Label>
           <Form.Control placeholder="Enter Phone Number" value={state.phone} onChange={(e) => setState({
@@ -47,7 +53,6 @@ const onSubmit = (event) => {
             phone: e.target.value
           })} />
         </Form.Group>
-
         <Form.Row>
           <Form.Group controlId="exampleForm.ControlTextarea1">
             <Form.Label className="text-white">Additional message:</Form.Label>
@@ -57,7 +62,6 @@ const onSubmit = (event) => {
             })} />
           </Form.Group>
         </Form.Row>
-
         <Button className="submit_color lead" type="submit">
           Submit
         </Button>
@@ -66,6 +70,4 @@ const onSubmit = (event) => {
     </div>
   );
 };
-
-
 export default ContactForm
